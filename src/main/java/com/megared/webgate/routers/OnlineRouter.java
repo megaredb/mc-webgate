c9dbd2de9c7094d36673634da07d91e9bc3a0a4c
megared <mredikf@gmail.com>
2023-12-05 09:34:53 +0200

    Removed unused imports from online router.

--- src/main/java/com/megared/webgate/routers/OnlineRouter.java
+++ src/main/java/com/megared/webgate/routers/OnlineRouter.java
@@ -1,23 +1,12 @@
 package com.megared.webgate.routers;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.janboerman.invsee.spigot.api.EnderSpectatorInventory;
-import com.janboerman.invsee.spigot.api.response.SpectateResponse;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.Server;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.json.simple.JSONObject;
 import rawhttp.core.RawHttp;
 import rawhttp.core.RawHttpRequest;
 import rawhttp.core.RawHttpResponse;
 import rawhttp.core.body.StringBody;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
